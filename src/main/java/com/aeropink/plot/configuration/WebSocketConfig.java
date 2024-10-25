3b312df4d8d09f6f347e74c8668b0f7240ad29ec
AntonioSalcido <antonio@bhernal.com>
2024-10-25 17:29:53 -0600

    feat: Se agrega el origen de github pages

--- src/main/java/com/aeropink/plot/configuration/WebSocketConfig.java
+++ src/main/java/com/aeropink/plot/configuration/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/createConnection").setAllowedOrigins("*")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("http://localhost:3000", "https://aeropink130.github.io")
                 .setHandshakeHandler(new CustomHandshakeHandler()) // Añadir el handshake handler
                 .withSockJS();
     }
